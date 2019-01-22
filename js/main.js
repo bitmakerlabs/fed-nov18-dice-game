47bd82e3f5f216e94f3f06a3e97aca06662ccdbf
juneate <roccopan@gmail.com>
2019-01-22 16:52:10 -0500

    Updated after class, week 08

--- js/main.js
+++ js/main.js
@@ -1,47 +1,58 @@
-// Step 2: Randomize a dice roll (make sure it's an integer number)
 
-// PEDMAS
-var dice = Math.floor( Math.random() * 6 ) + 1;
+// Write a function such that using the command
+// rollTheDice() will roll the dice (generate a new random roll)
 
+var rollTheDice = function() {
 
-console.log(`You rolled: ${dice}`);
-// test into the document
-
-
-// Step 3: Update the user interface (document) to show the diceface (svg image)
-
-var rollElement = document.getElementById('roll');
-var diceElement = document.getElementById('dice');
-
-var diceWord = '';
-
-if (dice == 1) {
-  diceWord = 'one'
-}
-else if (dice == 2) {
-  diceWord = 'two'
-}
-else if (dice == 3) {
-  diceWord = 'three'
-}
-else if (dice == 4) {
-  diceWord = 'four'
-}
-else if (dice == 5) {
-  diceWord = 'five'
-}
-else if (dice == 6) {
-  diceWord = 'six'
-}
+  // Step 2: Randomize a dice roll (make sure it's an integer number)
+  // PEDMAS
+  var dice = Math.floor( Math.random() * 6 ) + 1;
 
 
-rollElement.innerHTML = `You rolled: ${diceWord}`;
-// Change the src attribute
-diceElement.setAttribute('src', `img/dice${dice}.svg`);
-
-// This works too:
-// diceElement.src = `img/dice${dice}.svg`;
+  console.log(`You rolled: ${dice}`);
+  // test into the document
 
 
+  // Step 3: Update the user interface (document) to show the diceface (svg image)
+  var rollElement = document.getElementById('roll');
+  var diceElement = document.getElementById('dice');
 
-// Step 4: Convert the roll number (for example: 3) to a word ("three")
+  var diceWord = '';
+
+  if (dice == 1) {
+    diceWord = 'one';
+  }
+  else if (dice == 2) {
+    diceWord = 'two';
+  }
+  else if (dice == 3) {
+    diceWord = 'three';
+  }
+  else if (dice == 4) {
+    diceWord = 'four';
+  }
+  else if (dice == 5) {
+    diceWord = 'five';
+  }
+  else if (dice == 6) {
+    diceWord = 'six';
+  }
+  else {
+    diceWord = 'Woops!';
+  }
+
+
+  //rollElement.innerHTML = `You rolled: ${diceWord}`;
+  rollElement.innerHTML = diceWord;
+  // Change the src attribute
+  diceElement.setAttribute('src', `img/dice${dice}.svg`);
+
+
+};
+
+// Listen(er) for the event
+// who?
+// when?
+// then...
+
+document.getElementById('doroll').addEventListener('click', rollTheDice)
